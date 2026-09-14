7318a5eb7e838bb93655bcc49cf339d8731a17c6
epimediahub <epifanidaniel6@gmail.com>
2026-09-14 22:22:37 +0200

    Add parity EPG time formatter

---
+++ android/v0.4.0/app/src/main/java/de/epimediahub/app/ui/ParityTime.kt
@@ -0,0 +1,7 @@
+package de.epimediahub.app.ui
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+internal fun clock(s: Long): String = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date(s * 1000L))
